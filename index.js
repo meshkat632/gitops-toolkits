9286cd8554be585f5bbe83ef9411b771045ba1ed
Meshkatul Anwer <meshkat632@gmail.com>
2023-09-09 01:45:25 +0200

    release version:v1.2 at 20230909014524

--- index.js
+++ index.js
@@ -31,6 +31,17 @@ try {
         console.log("Output: \n", output)
     })
 
+    exec('git status', (err, output) => {
+        // once the command has completed, the callback function is called
+        if (err) {
+            // log and return if we encounter an error
+            console.error("could not execute command: ", err)
+            return
+        }
+        // log the output received from the command
+        console.log("Output: \n", output)
+    })
+
     const simpleGit = require('simple-git');
 
     /*
