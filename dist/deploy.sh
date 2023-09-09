bc64071c8ff0a9e35bb0a6c2da46fdce50fbe35d
Meshkatul Anwer <meshkat632@gmail.com>
2023-09-09 11:09:40 +0200

    release version:v1.2 at 20230909110939

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -12,36 +12,49 @@ while (( "$#" )); do
     shift
 done
 
-# Do the deployment
-echo "Hello, bucketname:${args[bucketname]}"
-
 BUILD_TIME=$(date '+%Y%m%d%H%M%S')
-echo "BUILD_TIME: $BUILD_TIME"
+tagName=${args[tagName]}
+sourceRepo=${args[sourceRepo]}
+sourceBranch=${args[sourceBranch]}
+targetRepo=${args[targetRepo]}
+targetBranch=${args[targetBranch]}
+GITHUB_TOKEN=${args[GITHUB_TOKEN]}
+GITHUB_USER=${args[GITHUB_USER]}
+
+
+echo "tagName: $tagName"
+echo "sourceRepo: $sourceRepo"
+echo "sourceBranch: $sourceBranch"
+echo "targetRepo: $targetRepo"
+echo "targetBranch: $targetBranch"
+echo "GITHUB_TOKEN: $GITHUB_TOKEN"
+echo "GITHUB_USER: $GITHUB_USER"
+
 mkdir -p "temp-$BUILD_TIME"
 
-
-git clone --branch deployments/argo-DEV https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY} "temp-$BUILD_TIME/vvc-portal"
-
+git clone --branch main https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/$targetRepo "temp-$BUILD_TIME/$targetRepo"
+cd temp-$BUILD_TIME/$targetRepo
+ls -ltr
 #git clone --branch master https://github.com/ververica/vvc-portal "temp-$BUILD_TIME/vvc-portal-master"
 
 
 
 #git clone --branch deployments/env-DEV https://github.com/ververica/vvc-portal "temp-$BUILD_TIME/vvc-portal-deployments-env-DEV"
-cd temp-$BUILD_TIME/vvc-portal
-echo "#############################"
-pwd
-git status
-echo "#############################"
+#cd temp-$BUILD_TIME/vvc-portal
+#echo "#############################"
+#pwd
+#git status
+#echo "#############################"
 #git diff deployments/env-DEV origin/master --color > diff.txt
 #cat diff.txt
-git fetch origin
-git reset --hard origin/deployments/pr-branch
-git clean -df
+#git fetch origin
+#git reset --hard origin/deployments/pr-branch
+#git clean -df
 #git commit -m "reset to pr-branch at $BUILD_TIME"
 #git push
-echo "#############################"
-pwd
-git status
+#echo "#############################"
+#pwd
+#git status
 #echo "#############################"
 #git push
 #git status
